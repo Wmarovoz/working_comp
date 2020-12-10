7003e2c8984b743a090603b6ee6b6216f0b62b87
Alex <edgarovi4po@gmail.com>
2020-12-10 17:16:37 +0200

    for Vitalik

--- file.py
+++ file.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python3
-print('sum')
+vol1 = []
+vol2= []
+numbers = [7, 2, 3, 8]
+numbers = sorted(numbers, reverse=True)
+sum_numbers = sum(numbers)
+half_numbers = sum_numbers / 2
+for i in numbers:
+    if i <= half_numbers:
+        vol1.append(i)
+        half_numbers -= i
+    else:
+        vol2.append(i)
+print(vol1)
+print(vol2)    
