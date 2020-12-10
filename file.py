287dd7f78e6a930d8f67fdccec0ad34ae8286548
Alex <edgarovi4po@gmail.com>
2020-12-10 17:43:28 +0200

    update with random

--- file.py
+++ file.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
+import random
+
+
 vol1 = []
 vol2= []
-numbers = [7, 2, 3, 8]
+numbers = [random.randint(0, 1000) for i in range(random.randint(10, 100))]
 numbers = sorted(numbers, reverse=True)
 sum_numbers = sum(numbers)
 half_numbers = sum_numbers / 2
+print(half_numbers)
 for i in numbers:
     if i <= half_numbers:
         vol1.append(i)
@@ -13,3 +17,5 @@ for i in numbers:
         vol2.append(i)
 print(vol1)
 print(vol2)    
+print(sum(vol1))
+print(sum(vol2))
